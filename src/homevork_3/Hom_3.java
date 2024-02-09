e8df23c22390e7e909ac0558482aa7fbcd45fe28
TolsVA <himtol@yandex.ru>
2024-02-09 16:24:23 +0700

    Редактирование сортировки слиянием

--- src/homevork_3/Hom_3.java
+++ src/homevork_3/Hom_3.java
@@ -4,54 +4,56 @@ import java.util.Arrays;
 
 public class Hom_3 {
     public static void main(String[] args) {
-
         Hom_3 hom = new Hom_3();
-
         System.out.println(Arrays.toString(hom.mergeSort(new int[]{9, 4,23, 45, 67,3, 4, 6, 5})));
-
     }
+
     public int[] mergeSort(int[] arr){
-        if (arr == null) {
-            return null;
-        }
+        if (arr == null) return null;
+        if (arr.length < 2) return arr;
 
-        if (arr.length < 2) {
-            return arr;
-        }
+        int[] arrLeft = new int[arr.length / 2];
+        System.arraycopy(arr, 0, arrLeft, 0, arr.length / 2);
 
-        int [] arrOne = new int[arr.length / 2];
-        System.arraycopy(arr, 0, arrOne, 0, arr.length / 2);
+        int[] arrRight = new int[arr.length - arr.length / 2];
+        System.arraycopy(arr, arr.length / 2, arrRight, 0, arr.length - arr.length / 2);
 
-        int [] arrTwo = new int[arr.length - arr.length / 2];
-        System.arraycopy(arr, arr.length / 2, arrTwo, 0, arr.length - arr.length / 2);
+        arrLeft = mergeSort(arrLeft);
+        arrRight = mergeSort(arrRight);
 
-        arrOne = mergeSort(arrOne);
-        arrTwo = mergeSort(arrTwo);
-
-        return mergeArray(arrOne, arrTwo);
+        return mergeArray(arrLeft, arrRight);
     }
 
-    public static int[] mergeArray(int[] a1, int[] a2) {
-        int[] res = new int[a1.length + a2.length];
-        int n = a1.length;
-        int m = a2.length;
+    public static int[] mergeArray(int[] arrLeft, int[] arrRight) {
+        int l = arrLeft.length;
+        int r = arrRight.length;
+        int[] res = new int[l + r];
+
         int i = 0, j = 0, k = 0;
-        while (i < n && j < m) {
-            if (a1[i] <= a2[j]) {
-                res[k] = a1[i];
-                i++; }
-            else {
-                res[k] = a2[j];
-                j++; }
-            k++; }
-        while (i < n) {
-            res[k] = a1[i];
+
+        while (i < l && j < r) {
+            if (arrLeft[i] <= arrRight[j]) {
+                res[k] = arrLeft[i];
+                i++;
+            } else {
+                res[k] = arrRight[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < l) {
+            res[k] = arrLeft[i];
             i++;
-            k++; }
-        while (j < m) {
-            res[k] = a2[j];
+            k++;
+        }
+
+        while (j < r) {
+            res[k] = arrRight[j];
             j++;
-            k++; }
+            k++;
+        }
+
         return res;
     }
 }
